82ea04cb87e7bd0a6fe796b6fd1a47d0538c461f
iwhore <145059805+iwhore@users.noreply.github.com>
2025-05-29 14:07:00 +0300

    portfolio

---
+++ src/html.js
@@ -0,0 +1,19 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+// import Accordion js
+import Accordion from 'accordion-js';
+// import Accordion styles
+import 'accordion-js/dist/accordion.min.css';
+
+import "./js/accordion";
+import "./js/menu";
+import "./js/modal";
+import "./js/swiper";
+import "./js/about-me"
+import "./js/faq";
+import "./js/api";
+import "./js/header"
+import "./js/rewies"
